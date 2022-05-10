75df9fe4f96ffadce94e982920da66e718824d5b
Manav Madan Rawal <80583498+Manav02012002@users.noreply.github.com>
2022-05-10 18:15:41 +0530

    Update DataScience_class2.R

--- DataScience_class2.R
+++ DataScience_class2.R
@@ -5,4 +5,14 @@ comp_data=read.csv("Computer_Data.csv")
 names(comp_data)
 nrow(comp_data)
 ncol(comp_data)
+dim(comp_data)
+head(comp_data)
+tail(comp_data)
+comp_data[10:20,]
+comp_data[20,3]
+comp_data[10]
+comp_data[c(1,2,3),]
+comp_data[c(10,15,20),c(1,5,8)]
+comp_data[,-c(1,3,5)]
+data2 = subset(comp_data,price>4000)
 
